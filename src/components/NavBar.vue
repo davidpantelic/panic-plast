c2815c8369b40bdabbd1188973220cc585df9117
David Pantelic <davidpantelic1996@gmail.com>
2024-10-09 07:54:18 +0200

    minor fix

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="#main-content" class="skip-content-link">Skip to main content</a>
+  <a href="#main-content" class="skip-content-link">{{ t('nav.skip-link') }}</a>
   <header>
     <nav :class="[isScrolling ? 'py-4' : 'py-5 bg-opacity-90']" class="bg-brand-black text-white fixed top-0 w-full flex justify-end px-6 lg:px-10 z-50">
 
